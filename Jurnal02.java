11a5f276be54a06752cf3f6c796ba9899efcde55
thessaroxana <thessaroxana3@gmail.com>
2021-10-01 16:20:32 +0700

    Solusi Jurnal 02

---
+++ Jurnal02.java
@@ -0,0 +1,8 @@
+public class Jurnal02 {
+	public static void main(String[] args) {
+		System.out.println("NIM  : 6706213025");
+		System.out.println("Nama : Thessa Roxana");
+		System.out.println("Kelas: D3IF-45-04");
+	}
+}
+		
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Jurnal Modul 02
 
-NIM: 
+NIM: 6706213025
 
-Nama: 
+Nama: Thessa Roxana
